e86c65e54971782ae6aad2fd16ff284f1b7faf7b
Nish Kakarala <nishman2020@yahoo.com>
2025-07-13 20:13:52 +0000

    latest and final version

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -18,12 +18,7 @@ import com.example.entity.Message;
 import com.example.service.AccountService;
 import com.example.service.MessageService;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
+
 
  @RestController
 public class SocialMediaController {
@@ -109,15 +104,5 @@ public class SocialMediaController {
 
 }
 
-/*
-As a user, I should be able to verify my login on the endpoint POST localhost:8080/login. 
-The request body will contain a JSON representation of an Account.
-
-- The login will be successful if and only if the username and password provided in the 
-request body JSON match a real account existing on the database. If successful, the response 
-body should contain a JSON of the account in the response body, including its accountId. 
-The response status should be 200 OK, which is the default.
-- If the login is not successful, the response status should be 401. (Unauthorized)
- */
 
 
